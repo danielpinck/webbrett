d70d31d89e418bb6bbf514423531be06be677de1
danielpinck <danielpinck@gmail.com>
2024-04-15 14:25:06 +0200

    mysql functionality add and select

---
+++ mysql_select_testing.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bfw_market"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT display_name, email, rolle, uid FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    echo "id: " . $row["uid"] . " - Benutzername: " . $row["display_name"] . " Rolle: " . $row["rolle"] . "E-mail: " . $row["email"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
